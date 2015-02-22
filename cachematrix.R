cd6e7d259741a46c79ec86053d9792802a6c49f7
pcrody <pcrody@live.com>
2015-02-22 09:40:39 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,6 +5,26 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+   cache <- NULL
+   ## set the value of the vector
+   setvect <- function(y)  {
+        x <<- y
+        cache <<- NULL
+        }
+   ## get the value of the vector
+   getvect <- function() x
+   ## set the value of the mean
+   setmean <- function(solve)  { 
+        cache <<- solve
+        }
+   ## get the value of the mean
+   getmean <- function() cache
+   ## list
+   list(setvect = setvect, getvect = getvect,
+   setmean = setmean
+   getmean = getmean)
+
+
 }
 
 
@@ -12,4 +32,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+
+   m <- x$getmean()
+        ## Check for values
+   if(!is.null(m))  {
+        message("getting cached data")
+        return(m)
+        }
+   ## Invert and cache
+   data <- x$get()
+   m <- solve(data, ...)
+   x$setmean(m)
+   m
+
 }
